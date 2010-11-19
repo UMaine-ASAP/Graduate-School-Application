4fcc49ccc2ce04c217ab37f8c64441b4e558f7c8
Josh Belanger <Josh Belanger@928ec8c1-dafb-41a4-8235-430f612261d0>
2010-11-19 21:48:19 +0000

    git-svn-id: svn+ssh://chitna.asap.um.maine.edu/var/svn/grad_application/trunk@30 928ec8c1-dafb-41a4-8235-430f612261d0

--- forms/signin/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	header("Location: ../../../application/pages/login.php")
-	// This is the old location of the login page, but some pages on the drupal site still link here.
-	// Until the links are changed to the new location, which is /application/pages/login.php,
-	// this redirect should stay here.
-?>
