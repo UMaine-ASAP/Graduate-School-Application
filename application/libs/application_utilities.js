ae851ede4a6da12302b4533e95c2af614b4ee64f
Josh Belanger <Josh Belanger@928ec8c1-dafb-41a4-8235-430f612261d0>
2010-12-16 01:40:30 +0000

    git-svn-id: svn+ssh://chitna.asap.um.maine.edu/var/svn/grad_application/trunk@54 928ec8c1-dafb-41a4-8235-430f612261d0

--- application/libs/application_utilities.js
+++
@@ -1,11 +0,0 @@
-/*
-	toggleVisibility
-	
-	Radio buttons toggle field visibility
-	Used to reveal futher information fields based on radio button selection.
-	
-	need to edit document.gradform??
-	
-*/
-
-<script type="text/javascript">    function toggleVisibility(elementID) {         var theDiv = document.getElementById(elementID);         if (document.gradform.RadioGroup1_0.checked == true) {            theDiv.style.display = 'block';        } else {            theDiv.style.display = 'none';        }    }</script>
