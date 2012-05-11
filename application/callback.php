6755613b40bb9c7c542b977856a832bf516e37d3
asap <asap@928ec8c1-dafb-41a4-8235-430f612261d0>
2012-05-11 17:12:38 +0000

    some of callback script programmed - needs more work
    git-svn-id: svn+ssh://chitna.asap.um.maine.edu/var/svn/grad_application/trunk@179 928ec8c1-dafb-41a4-8235-430f612261d0

--- application/callback.php
+++ application/callback.php
@@ -6,7 +6,14 @@ require_once "libs/variables.php";
 $db = new Database();
 $db->connect();
 
+function writeData($file, $stringData) {
+	$fh = fopen($file, 'a') or die("can't open file");
+	fwrite($fh, $stringData);
+	fclose($fh);
+}
+
 # Parse POST data from Touchnet
+$key = $_REQUEST['posting_key'];
 $status = $_REQUEST['pmt_status'];
 $trans_id = $_REQUEST['EXT_TRANS_ID'];
 $identifier_array = explode("*", $trans_id);
@@ -14,19 +21,32 @@ $upaysiteID = $identifier_array[0];
 $applicantID = $identifier_array[1];
 $transID = $identifier_array[2];
 
+$payment_method = "";
+if ($status == 'success') {
+
+	if( isset( $_REQUEST['card_type'] )) {
+		$payment_method = "CREDIT";
+	} else {
+		$payment_method = "ACH";	
+	}
+
+}
+
+
+
+writeData("log.txt", $status . "\n");
+writeData("log.txt", json_encode($_REQUEST) . "\n");
+
 # IF transaction was successful:
 if ($status == "success") {
 
-	$app_msg = "success";
 	$db->iquery("UPDATE applicants SET application_fee_payment_status='Y' WHERE applicants.applicant_id=%d", $applicantID);
 	$db->iquery("UPDATE applicants SET application_fee_transaction_date='%s' WHERE applicants.applicant_id=%d", date("Y-m-d"), $applicantID);
 	$db->iquery("UPDATE applicants SET application_fee_transaction_type='Online' WHERE applicants.applicant_id=%d", $applicantID);
 
+	$db->iquery("UPDATE applicants SET application_fee_transaction_payment_method='%s' WHERE applicants.applicant_id=%d", $payment_method, $applicantID);
+
 	$db->close();
-
-	# Update Proxy DB
-
-	include_once("return-callback.php");
 
 } else {
 	$db->close();
