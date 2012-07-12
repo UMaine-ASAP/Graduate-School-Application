6f16581315e49db5f93f53f2b2ccee786033f68d
Tim Baker <timbone945@gmail.com>
2012-07-12 15:29:32 -0400

    fix Callback script. Adds request from Janet to include payment type (CREDIT or ACH)

--- application/callback.php
+++ application/callback.php
@@ -6,23 +6,29 @@ require_once "libs/variables.php";
 $db = new Database();
 $db->connect();
 
-function writeData($file, $stringData) {
-	$fh = fopen($file, 'a') or die("can't open file");
-	fwrite($fh, $stringData);
-	fclose($fh);
-}
-
-# Parse POST data from Touchnet
-$key = $_REQUEST['posting_key'];
-$status = $_REQUEST['pmt_status'];
+// Parse data from Touchnet
+$key 	  = $_REQUEST['posting_key'];
+$status   = $_REQUEST['pmt_status'];
 $trans_id = $_REQUEST['EXT_TRANS_ID'];
-$identifier_array = explode("*", $trans_id);
-$upaysiteID = $identifier_array[0];
-$applicantID = $identifier_array[1];
-$transID = $identifier_array[2];
 
-$payment_method = "";
-if ($status == 'success') {
+$identifier_array = explode("*", $trans_id);
+
+$upaysiteID  = $identifier_array[0];
+$applicantID = $identifier_array[1];
+$transID 	 = $identifier_array[2];
+
+$result = $db->query('SELECT application_fee_transaction_number FROM applicants WHERE applicant_id=%d', $applicantID);
+$stored_transaction_id = $result[0];
+
+// Process successful payments
+if ($status == "success" 
+	&& $key == $GLOBALS['touchnet_posting_key']
+	&& $stored_transaction_id == $trans_id
+	) 
+{
+
+	// Set Payment method
+	$payment_method = "";
 
 	if( isset( $_REQUEST['card_type'] )) {
 		$payment_method = "CREDIT";
@@ -30,26 +36,16 @@ if ($status == 'success') {
 		$payment_method = "ACH";	
 	}
 
-}
-
-
-
-writeData("log.txt", $status . "\n");
-writeData("log.txt", json_encode($_REQUEST) . "\n");
-
-# IF transaction was successful:
-if ($status == "success") {
-
+	// Update Database
 	$db->iquery("UPDATE applicants SET application_fee_payment_status='Y' WHERE applicants.applicant_id=%d", $applicantID);
 	$db->iquery("UPDATE applicants SET application_fee_transaction_date='%s' WHERE applicants.applicant_id=%d", date("Y-m-d"), $applicantID);
 	$db->iquery("UPDATE applicants SET application_fee_transaction_type='Online' WHERE applicants.applicant_id=%d", $applicantID);
 
 	$db->iquery("UPDATE applicants SET application_fee_transaction_payment_method='%s' WHERE applicants.applicant_id=%d", $payment_method, $applicantID);
 
-	$db->close();
-
-} else {
-	$db->close();
 }
+
+$db->close();
+
 
 ?>
