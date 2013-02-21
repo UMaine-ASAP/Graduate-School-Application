291781022cd35c86375f2a38c6ff827ad6d3b4ee
Tim Westbaker <timbone945@gmail.com>
2013-02-20 19:17:08 -0500

    Adds all? of the missing database config fields

--- application/models/databaseConfig.php
+++ application/models/databaseConfig.php
@@ -2,6 +2,16 @@
 
 $databaseFields = array(
 
+	// Application
+	'startYear'                   => 'filter_generic', 	// @TODO: filter using options
+	'startSemester'               => 'filter_semester', 	// @TODO: filter using options
+	'desiredHousing'              => 'filter_generic', 	// @TODO: filter using options
+	'waiveReferenceViewingRights' => 'filter_boolean',
+	'hasUmaineCorrespondent'      => 'filter_boolean',
+	'umaineCorrespondentDetails'  => 'filter_generic',
+	'hasAcceptedTermsOfAgreement' => 'filter_boolean',
+
+
 	// Personal
 	'personal-givenName'      => 'filter_generic',
 	'personal-middleName'     => 'filter_generic',
@@ -58,6 +68,7 @@ $databaseFields = array(
 	'personal-prevUMGradWithdraw_exists' => 'filter_boolean',
 	'personal-prevUMGradWithdraw_date'   => 'filter_short_date',
 
+
 	// Ethnicity
 	// TODO these should probably be booleans? 
 	'personal-ethnicity_hispa'  => 'equals_HISPA',
@@ -68,28 +79,46 @@ $databaseFields = array(
 	'personal-ethnicity_white'  => 'equals_white',
 	'personal-ethnicity_unspec' => 'equals_unspec',
 
+
 	// Language
 	'personal-isEnglishPrimary'     => 'filter_boolean',
+
 	'language-language'             => 'filter_generic',
-	'language-proficiency_writing'  => 'filter_proficiency',
-	'language-proficiency_reading'  => 'filter_proficiency',
-	'language-proficiency_speaking' => 'filter_proficiency',
+	'language-proficiency_writing'  => 'filter_proficiency', // @TODO: filter using options
+	'language-proficiency_reading'  => 'filter_proficiency', // @TODO: filter using options
+	'language-proficiency_speaking' => 'filter_proficiency', // @TODO: filter using options
+
 
 	// International
 	'international-isInternationalStudent' => 'filter_boolean',
-	'international-toefl_hasTaken'         => 'filter_boolean',
-	'international-toefl_hasReported'      => 'filter_boolean',
-	'international-toefl_date'             => 'filter_short_date',
 
-	'international-toefl_score'                     => 'filter_toefl_score',
+	'international-toefl_hasTaken'    => 'filter_boolean',
+	'international-toefl_hasReported' => 'filter_boolean',
+	'international-toefl_date'        => 'filter_short_date',
+	'international-toefl_score'       => 'filter_toefl_score',
+
+	'international-hasUSCareer'     => 'filter_boolean',
+	'international-usCareerDetails' => 'filter_generic',
+
+	'international-hasFurtherStudies'     => 'filter_boolean',
+	'international-furtherStudiesDetails' => 'filter_generic',
+
+	'international-hasHomeCareer'     => 'filter_boolean',
+	'international-homeCareerDetails' => 'filter_generic',
+
+	'international-financeDetails' => 'filter_generic',
+
+	'international-usEmergencyContact_name'         => 'filter_generic',
+	'international-usEmergencyContact_relationship' => 'filter_relationship', // @TODO: filter using options	
 	'international-usEmergencyContact-primaryPhone' => 'filter_phone',
-	'international-usEmergencyContact-state'        => 'filter_state',	
+	'international-usEmergencyContact-state'        => 'filter_state',
 	'international-usEmergencyContact-zip'          => 'filter_zipcode',
-	'international-homeEmergencyContact-phone'      => 'filter_phone',
-	'international-homeEmergencyContact-country'    => 'filter_country',
-	'international-hasFurtherStudies'               => 'filter_boolean',
-	'international-hasUSCareer'                     => 'filter_boolean',
-	'international-hasHomeCareer'                   => 'filter_boolean',
+
+	'international-homeEmergencyContact_name'         => 'filter_generic',
+	'international-homeEmergencyContact_relationship' => 'filter_relationship', // @TODO: filter using options	
+	'international-homeEmergencyContact-primaryPhone' => 'filter_phone',
+	'international-homeEmergencyContact-country'      => 'filter_country',
+
 
 	// Previous School
 	'previousSchool-name'              => 'filter_generic',
@@ -103,30 +132,41 @@ $databaseFields = array(
 	'previousSchool-degreeEarned_name' => "filter_generic",
 	'previousSchool-degreeEarned_date' => "filter_short_date",
 
+
 	//GRE
-	'gre-hasTaken'        => 'filter_boolean',
-	'gre-date'            => 'filter_short_date',
-	'gre-verbal'          => 'filter_gre_verbal',
-	'gre-quantitative'    => 'filter_gre_quantitative',
-	'gre-analytical'      => 'filter_gre_analytical',
-	'gre-subject'         => 'filter_gre_subject',
-	'gre-hasBeenReported' => 'filter_boolean',
-	'gre-score'           => 'filter_gre_score',
+	'personal-hasTakenGRE' => 'filter_boolean',
+	'gre-date'             => 'filter_short_date',
+	'gre-verbal'           => 'filter_gre_verbal',
+	'gre-quantitative'     => 'filter_gre_quantitative',
+	'gre-analytical'       => 'filter_gre_analytical',
+	'gre-subject'          => 'filter_gre_subject',
+	'gre-hasBeenReported'  => 'filter_boolean',
+	'gre-score'            => 'filter_gre_score',
+
 
 	// degree
-	'degree-academic_program' => 'filter_generic',
-	'degree-academic_major' => 'filter_generic',	
-	'degree-academic_major' => 'filter_generic',
-	'degree-academic_minor' => 'filter_generic',
+	'degree-academic_program'                 => 'filter_generic', // @TODO: filter using options
+	'degree-academic_plan'                    => 'filter_generic', // @TODO: filter using options
+	'degree-academic_major'                   => 'filter_generic',
+	'degree-academic_minor'                   => 'filter_generic',
+	'degree-academic_load'                    => 'filter_generic', // @TODO: filter using options
+	'degree-studentType'                      => 'filter_generic',	// @TODO: filter using options
+	'degree-isSeekingFinancialAid'            => 'filter_boolean',
+	'degree-isSeekingAssistantship'           => 'filter_boolean',
+	// 'degree-desiredAssitantshipDepartment' => 'filter_generic', // disabled for now
+	'degree-isApplyingNebhe'                  => 'filter_boolean',
+
 
 	// References
-	'reference-firstName'           => 'filter_name',
-	'reference-lastName'            => 'filter_name',
-	'reference-email'               => 'filter_email',
-	'reference-relationship'        => 'filter_relationship',
-	'reference-isSubmittingOnline'  => 'filter_boolean',
-	'reference-requestHasBeenSent'  => 'filter_boolean',
-	'reference-submittedDate'       => 'filter_long_date',
+	'reference-firstName'          => 'filter_name',
+	'reference-lastName'           => 'filter_name',
+	'reference-email'              => 'filter_email',
+	'reference-relationship'       => 'filter_relationship',
+	'reference-isSubmittingOnline' => 'filter_boolean',
+	'reference-requestHasBeenSent' => 'filter_boolean',
+	'reference-submittedDate'      => 'filter_long_date',
+	'reference-filename'           => 'filter_generic',	
+	
 	'reference-phone'               => 'filter_phone',
 	'reference-state'               => 'filter_state',
 	'reference-postal'              => 'filter_zipcode',
@@ -135,22 +175,17 @@ $databaseFields = array(
 	'reference-englishYearsUniv'    => 'filter_date_range',
 	'reference-englishYearsPrivate' => 'filter_date_range',
 
-	'degree-attendanceLoad'         => 'filter_attendance_load',
-	'degree-studentType'            => 'filter_student_type',
-	'degree-isSeekingAssistantship' => 'filter_boolean',
-	'degree-isApplyingNebhe'        => 'filter_boolean',
 
-	'application-startYear'                   => 'filter_generic',
-	'application-startSemester'               => 'filter_semester',
-	'application-hasUmaineCorrespondent'      => 'filter_boolean',
-	'application-waiveReferenceViewingRights' => 'filter_boolean',
-	'application-asAcceptedTermsOfAgreement'  => 'filter_boolean',
-
-
+	// Disciplinary Violation
 	'disciplinaryViolation-exists' => 'filter_boolean',
-	'disciplinaryViolation-date'   => 'filter_short_date',
 
+	'disciplinaryViolation-date'    => 'filter_short_date',
+	'disciplinaryViolation-details' => 'filter_generic',
+
+
+	// Civil Violation
 	'criminalViolation-exists' => 'filter_boolean',
-	'criminalViolation-date'   => 'fileter_date'
 
+	'criminalViolation-date'    => 'fileter_date',
+	'criminalViolation-details' => 'fileter_date'
 );
